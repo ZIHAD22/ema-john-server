194c1132db75ed1d2415d4751c821a70bcc52293
ZIHAD22 <mdzihad2244@gmail.com>
2022-04-28 02:28:32 +0600

    add many think

--- index.js
+++ index.js
@@ -9,6 +9,49 @@ const app = express()
 
 // middleware
 app.use(cors())
+app.use(express.json())
+
+// dataBase
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eyeab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+const dataBaseConnect = async () => {
+  try {
+    await client.connect()
+    const productCollection = client.db('emaJohn').collection('product')
+
+    app.get('/product', async (req, res) => {
+      const { page, size } = req.query
+      const query = {}
+      const curser = productCollection.find(query)
+      let product
+      if (page || size) {
+        product = await curser
+          .skip(parseInt(page) * parseInt(size))
+          .limit(parseInt(size))
+          .toArray()
+      } else {
+        product = await curser.toArray()
+      }
+
+      res.send(product)
+    })
+
+    app.get('/productCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount()
+
+      res.send({ count })
+    })
+  } finally {
+    console.log('dataBase is connected')
+  }
+}
+
+dataBaseConnect().catch(console.dir)
 
 // home router
 app.get('/', (req, res) => {
